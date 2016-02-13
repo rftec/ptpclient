5d6d86e71b6fc1fa55f82eb8dd907ff4b22d5e9c
rftec <rftec@users.noreply.github.com>
2016-02-14 01:46:08 +0200

    Added start() and stop() to sample python script

--- ptpclient.py
+++ ptpclient.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python
 from pyptp import Camera
+from time import sleep
+from os import mkdir
 
 VID_SONY = 0x054C
 PID_SONY_A6000 = 0x094E
 
 def main():
-	cam = Camera(vid=VID_SONY, pid=PID_SONY_A6000)
+	try:
+		mkdir('images')
+	except OSError:
+		pass
+	
+	cam = Camera(vid=VID_SONY, pid=PID_SONY_A6000, image_dir='images')
 	cam.setparams(drive='low')
 	#cam.setparams(iso=1000)
 	#cam.setparams(shutter=(1, 2000))
+	cam.start()
+	sleep(10)
+	cam.stop()
 	
 if __name__ == '__main__':
 	main()
